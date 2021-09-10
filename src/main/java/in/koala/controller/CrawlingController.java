d9d888da8013884fa09a926c4858f649bd117970
Noah981107 <shgustmd6865@naver.com>
2021-09-10 20:41:42 +0900

    Drom Crawling

---
+++ src/main/java/in/koala/controller/CrawlingController.java
@@ -0,0 +1,19 @@
+package in.koala.controller;
+
+import in.koala.service.CrawlingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class CrawlingController {
+
+    @Resource(name = "crawlingServiceImpl")
+    private CrawlingService crawlingService;
+
+    @GetMapping(value = "/dorm")
+    public void dormCrawling() throws Exception{
+        crawlingService.dormCrawling();
+    }
+}
---
+++ src/main/java/in/koala/service/CrawlingService.java
@@ -0,0 +1,6 @@
+package in.koala.service;
+
+public interface CrawlingService {
+
+    public void dormCrawling() throws Exception;
+}
