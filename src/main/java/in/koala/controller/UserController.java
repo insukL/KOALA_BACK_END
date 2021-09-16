b5df6c307d324068f1cba74ad1e898ba1c33b857
Noah981107 <shgustmd6865@naver.com>
2021-09-16 23:27:04 +0900

    Add Naver Log In Servie

---
+++ src/main/java/in/koala/controller/UserController.java
@@ -0,0 +1,36 @@
+package in.koala.controller;
+
+import in.koala.domain.naverLogin.NaverCallBack;
+import in.koala.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Resource(name = "userServiceImpl")
+    private UserService userService;
+
+    @GetMapping(value = "/test")
+    public String test(){
+        return userService.test();
+    }
+
+    @GetMapping(value =  "/login/naver")
+    public ResponseEntity<Map<String, String>> naverLogin(NaverCallBack callBack) throws IOException {
+        if(callBack.getError() != null){
+            throw new IOException(callBack.getError());
+        }
+        else{
+            return new ResponseEntity<>(userService.naverLogin(callBack), HttpStatus.OK);
+        }
+    }
+}
