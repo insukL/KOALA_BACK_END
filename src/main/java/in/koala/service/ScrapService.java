98d8e2e45a1ad5816fb0f574664f0b53c1ac8bfb
오준서 <48210061+5junseo@users.noreply.github.com>
2021-11-06 13:10:19 +0900

    memo, scrap

--- src/main/java/in/koala/service/ScrapService.java
+++ src/main/java/in/koala/service/ScrapService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ScrapService {
     void Scrap(Scrap scrap) throws Exception;
+    List<Board> getScrap() throws Exception;
     void deleteScrap(Long boardId) throws Exception;
     void deleteAllScrap(Long userId) throws Exception;
-    List<Board> getScrap() throws Exception;
 }
