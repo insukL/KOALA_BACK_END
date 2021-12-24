f34501c579bc9df9ac764f2fde68db7667833bde
yhsim98 <2008qwe123@gmail.com>
2021-12-24 12:49:52 +0900

    add login test file

--- src/main/java/in/koala/controller/sample/SnsLoginTestController.java
+++ src/main/java/in/koala/controller/sample/SnsLoginTestController.java
@@ -1,10 +1,15 @@
 package in.koala.controller.sample;
 
+import in.koala.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class SnsLoginTestController {
 
     private final String AUD = "com.koala.services";
@@ -28,5 +33,11 @@ public class SnsLoginTestController {
     @GetMapping(value = "/callback")
     public String naverCallBack(){
         return "test/callback";
+    }
+
+    @GetMapping(value = "/applecallback")
+    @ResponseBody
+    public String appleCallBack(String idToken){
+        return idToken;
     }
 }
