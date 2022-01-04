e4d5dc6f9302f23a4fb6f0b498920e51fbf83ed6
yhsim98 <2008qwe123@gmail.com>
2022-01-05 02:19:20 +0900

    edit enum code

--- src/main/java/in/koala/enums/EmailType.java
+++ src/main/java/in/koala/enums/EmailType.java
@@ -1,15 +1,5 @@
 package in.koala.enums;
 
-import lombok.Getter;
-
-@Getter
 public enum EmailType {
-    ACCOUNT(0), PASSWORD(1), UNIVERSITY(2);
-
-    private final int emailType;
-
-    EmailType(int emailType){
-        this.emailType = emailType;
-    }
-
+    ACCOUNT, PASSWORD, UNIVERSITY;
 }
--- src/main/java/in/koala/enums/TokenType.java
+++ src/main/java/in/koala/enums/TokenType.java
@@ -6,18 +6,13 @@ import java.util.Date;
 public enum TokenType {
     ACCESS(12), REFRESH(24*14), SNS(0);
 
-    private final Date tokenExp;
+    private int tokenRemainTime;
 
     TokenType(int tokenRemainTime) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-
-        calendar.add(Calendar.HOUR_OF_DAY, tokenRemainTime);
-
-        tokenExp = calendar.getTime();
+        this.tokenRemainTime = tokenRemainTime;
     }
 
-    public Date getTokenExp() {
-        return tokenExp;
+    public int getTokenRemainTime() {
+        return tokenRemainTime;
     }
 }
