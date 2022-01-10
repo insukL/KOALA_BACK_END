33786514ea65d61ae4d2d4209980ada5ce35293f
PDJ107 <cxz541@gmail.com>
2022-01-10 11:08:50 +0900

    Add: 크롤링 토큰 CRUD 추가

---
+++ src/main/java/in/koala/domain/CrawlingToken.java
@@ -0,0 +1,24 @@
+package in.koala.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import in.koala.annotation.ValidationGroups;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CrawlingToken {
+
+    private Long id;
+
+    @Length(message="설명은 1 ~ 200자 사이여야 합니다", min=1, max=200, groups = {ValidationGroups.createCrawlingToken.class, ValidationGroups.updateCrawlingToken.class})
+    private String description;
+
+    private Integer site;
+
+    @Length(message="토큰은 1 ~ 300자 사이여야 합니다", min=1, max=300, groups = {ValidationGroups.createCrawlingToken.class, ValidationGroups.updateCrawlingToken.class})
+    private String token;
+}
