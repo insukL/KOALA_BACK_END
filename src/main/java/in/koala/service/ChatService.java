b9298e367df47edd193af718e1e1c2fa0b36c4c8
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-02-21 16:01:05 +0900

    feat : Add Chat Image Upload fix : Chat Message Sent At Field
    채팅 이미지 전송 추가
    채팅 메시지 전송 시각 미전송 오류 해결

--- src/main/java/in/koala/service/ChatService.java
+++ src/main/java/in/koala/service/ChatService.java
@@ -2,13 +2,15 @@ package in.koala.service;
 
 import in.koala.domain.ChatMessage;
 import in.koala.domain.Criteria;
+import in.koala.domain.user.NormalUser;
 import org.springframework.messaging.Message;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ChatService {
     void send(Message<ChatMessage> message);
-    void imageSend();
+    String imageSend(MultipartFile multipartFile);
     String getMemberCount();
     String getMemberName(Long id);
     List<ChatMessage> getMessageList(Criteria criteria);
