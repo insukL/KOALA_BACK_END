81c40d2a44636379bee3cea6ea9a3bbdf1853f13
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-03-20 00:43:07 +0900

    feat : Delete Topic Base FCM Service
    주제 기반 FCM 서비스 메소드 삭제

--- src/main/java/in/koala/controller/FcmTestController.java
+++ src/main/java/in/koala/controller/FcmTestController.java
@@ -1,15 +1,8 @@
 package in.koala.controller;
 
-import in.koala.annotation.Auth;
 import in.koala.domain.Crawling;
-import in.koala.domain.DeviceToken;
-import in.koala.domain.Keyword;
-import in.koala.controller.response.BaseResponse;
-import in.koala.service.UserService;
-import in.koala.serviceImpl.DeviceTokenTestServiceImpl;
 import in.koala.serviceImpl.KeywordPushServiceImpl;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Authorization;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,35 +16,6 @@ public class FcmTestController {
     @Resource
     private KeywordPushServiceImpl keywordPushService;
 
-    @Resource
-    private DeviceTokenTestServiceImpl tokenService;
-
-    @Resource
-    private UserService userService;
-
-    @Auth
-    @PostMapping("/device")
-    @ApiOperation(value ="디바이스 토큰 등록 테스트" , notes = "디바이스 토큰 등록 실사용X" , authorizations = @Authorization(value = "Bearer +accessToken"))
-    public ResponseEntity insertDeviceToken(@RequestBody DeviceToken deviceToken){
-        tokenService.insertDeviceToken(deviceToken, userService.getLoginUserInfo().getId());
-        return new ResponseEntity<String>("success", HttpStatus.OK);
-    }
-
-    @Auth
-    @PostMapping("/sub")
-    @ApiOperation(value ="키워드 구독 테스트" , notes = "키워드 구독 실사용X" , authorizations = @Authorization(value = "Bearer +accessToken"))
-    public ResponseEntity subscribeKeyword(@RequestBody Keyword keyword) throws Exception {
-        keywordPushService.subscribe(keyword, userService.getLoginUserInfo().getId());
-        return new ResponseEntity<String>("success", HttpStatus.OK);
-    }
-
-    @Auth
-    @PostMapping("/unsub")
-    @ApiOperation(value ="키워드 구독 해제 테스트", notes = "키워드 구독 해제 실사용X", authorizations = @Authorization(value = "Bearer +accessToken"))
-    public ResponseEntity unsubscribeKeyword(@RequestBody Keyword keyword) throws Exception{
-        keywordPushService.unsubscribe(keyword, userService.getLoginUserInfo().getId());
-        return new ResponseEntity<String>("success", HttpStatus.OK);
-    }
 
     @PostMapping("/keyword")
     @ApiOperation(value ="키워드 푸시 테스트", notes = "키워드 알람 발송 실사용X")
@@ -60,36 +24,5 @@ public class FcmTestController {
                                       @RequestParam String keyword)throws Exception{
         keywordPushService.pushNotification(tokens, keyword, crawling);
         return new ResponseEntity<String>("success", HttpStatus.OK);
-    }
-
-    @Auth
-    @PostMapping("/new-sub")
-    @ApiOperation(value = "키워드 구독 수정 테스트", notes = "키워드 구독 수정 실사용X", authorizations = @Authorization(value = "Bearer +accessToken"))
-    public ResponseEntity modifySubscription(@RequestBody Keyword keyword) throws Exception{
-//        Keyword old = new Keyword();
-//        old.setName(keyword.getName());
-//        List<CrawlingSite> list = new ArrayList<>();
-//        Set<Integer> temp = keywordMapper.getKeywordSite(userService.getLoginUserInfo().getId(), old.getName());
-//        for(Integer t : temp){
-//            for(CrawlingSite c : CrawlingSite.values()){
-//                if(c.getCode() == t) list.add(c);
-//            }
-//        }
-//        old.setSiteList(list);
-//        keywordPushService.modifySubscription(old, keyword, userService.getLoginUserInfo().getId());
-        return new ResponseEntity<String>("success", HttpStatus.OK);
-    }
-
-    // --------------------------------- 위 인석 / 아래 현승 -------------------------------------------------------
-    // To. 인석
-    // 크롤링한 시점에서 키워드 푸쉬 될 수 있도록 변경해서 쓸게....
-    // KeywordPushController를 삭제하고 여기서 한번에 테스트 하려구
-
-    @Auth
-    @PostMapping("/keyword2")
-    @ApiOperation(value ="키워드 푸시 테스트2", notes = "키워드 알람 발송 실사용X", authorizations = @Authorization(value = "Bearer +accessToken"))
-    public ResponseEntity pushKeywordAtOnce() throws Exception{
-        keywordPushService.pushKeywordAtOnce();
-        return new ResponseEntity(BaseResponse.of("success", HttpStatus.OK), HttpStatus.OK);
     }
 }
--- src/main/java/in/koala/serviceImpl/KeywordPushServiceImpl.java
+++ src/main/java/in/koala/serviceImpl/KeywordPushServiceImpl.java
@@ -7,36 +7,18 @@ import com.google.firebase.messaging.MulticastMessage;
 import com.google.firebase.messaging.Notification;
 import in.koala.domain.Crawling;
 import in.koala.domain.Keyword;
-import in.koala.domain.fcm.ConditionMessage;
 import in.koala.enums.CrawlingSite;
-import in.koala.mapper.DeviceTokenTestMapper;
-import in.koala.mapper.KeywordPushMapper;
-import in.koala.mapper.NoticeMapper;
-import in.koala.service.CrawlingService;
 import in.koala.service.KeywordPushService;
-import in.koala.service.UserService;
-import in.koala.util.EnConverter;
-import in.koala.util.FcmSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
-import java.util.*;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class KeywordPushServiceImpl implements KeywordPushService {
-    private final UserService userService;
-    private final FcmSender fcmSender;
-    private final KeywordPushMapper keywordPushMapper;
-    private final CrawlingService crawlingService;
-    private final NoticeMapper noticeMapper;
-    private final EnConverter enConverter;
-    private final DeviceTokenTestMapper tokenMapper;
-
-
     @Value("${alarm.image.url}")
     private String iconUrl;
 
@@ -71,144 +53,17 @@ public class KeywordPushServiceImpl implements KeywordPushService {
         }
     }
 
-
-    //TODO : 아래 메소드 2개 정리
-    @Override
-    public void pushKeywordAtOnce() throws Exception {
-
-        crawlingService.executeAll();
-
-        Timestamp latelyCrawlingTime = crawlingService.getLatelyCrawlingTime();
-
-        System.out.println(latelyCrawlingTime);
-
-        List<Map<String, String>> tmp = keywordPushMapper.pushKeywordByLatelyCrawlingTime(latelyCrawlingTime);
-
-        System.out.println(tmp);
-
-        for(Map value : tmp){
-            StringBuilder sb = new StringBuilder();
-            ConditionMessage message = new ConditionMessage("키워드의 글이 등록되었습니다.",
-                    value.get("title").toString(),
-                    value.get("url").toString(),
-                    null);
-
-            sb.append("\'" + enConverter.ktoe(value.get("name").toString()) + "1" + "\' in topics");
-            message.setCondition(sb.toString());
-            System.out.println("최종 : "+ sb);
-            fcmSender.sendMessage(message);
-            noticeMapper.insertNotice(value);
-        }
-    }
-
-    // ---------------------------------------- 위 현승 / 아래 인석 -------------------------------------------------
-    /*
-    //Topic Message 활용안
-    @Override
-    public void pushKeyword(List<String> keyword, Crawling crawling) throws Exception{
-        TopicMessage message = new TopicMessage("키워드의 글이 등록되었습니다.",
-                                                    crawling.getTitle(),
-                                                    crawling.getUrl(),
-                                                    null );
-        for(String word : keyword){
-            message.setTopic(enConverter.ktoe(word) + crawling.getSite().toString());
-            fcmSender.sendMessage(message);
-        }
-    }
-     */
-
-    @Override
-    public void pushKeyword(List<String> keyword, Crawling crawling) throws Exception{
-        StringBuilder sb = new StringBuilder();
-        ConditionMessage message = new ConditionMessage("키워드의 글이 등록되었습니다.",
-                                                    crawling.getTitle(),
-                                                    crawling.getUrl(),
-                                                    null );
-        short cnt = 0;
-        for(String word : keyword){
-            if(cnt >= 5){
-                message.setCondition(sb.toString());
-                fcmSender.sendMessage(message);
-                sb.setLength(0);
-                cnt = 0;
-            }
-            if(cnt > 0) sb.append(" || ");
-            sb.append("\'" + enConverter.ktoe(word) + crawling.getSite().toString() + "\' in topics");
-            ++cnt;
-        }
-        message.setCondition(sb.toString());
-        fcmSender.sendMessage(message);
-    }
-
-    //Crawling Json 파싱 실패에 따른 테스트용 메소드
-    @Override
-    public void pushKeyword(List<String> keyword, String title, String url, Short site) throws Exception{
-        StringBuilder sb = new StringBuilder();
-        ConditionMessage message = new ConditionMessage("키워드의 글이 등록되었습니다.",
-                                                            title,
-                                                            url,
-                                                            null );
-        short cnt = 0;
-        for(String word : keyword){
-            if(cnt >= 5){
-                message.setCondition(sb.toString());
-                System.out.println("다섯개 이상일때 : " + sb);
-                fcmSender.sendMessage(message);
-                sb.setLength(0);
-                System.out.println("cnt>= 5 : " + sb);
-                cnt = 0;
-            }
-            if(cnt > 0) sb.append(" || ");
-            sb.append("\'" + enConverter.ktoe(word) + site + "\' in topics");
-            ++cnt;
-            System.out.println("cnt>0 : " + sb);
-        }
-        message.setCondition(sb.toString());
-        System.out.println("최종 : "+ sb);
-        fcmSender.sendMessage(message);
-    }
-
     @Override
     public void subscribe(Keyword keyword, Long id) throws Exception{
-        //UserMapper 임시 사용
-        List<String> tokenList = tokenMapper.getDeviceTokenById(id);
-        for(CrawlingSite site : keyword.getSiteList()){
-            fcmSender.subscribe(tokenList,
-                    enConverter.ktoe(keyword.getName()) + site.getCode().toString());
-            System.out.println(enConverter.ktoe(keyword.getName()) + site.getCode().toString());
-        }
     }
 
     @Override
     public void unsubscribe(Keyword keyword, Long id) throws Exception{
-        //UserMapper 임시 사용
-        List<String> tokenList = tokenMapper.getDeviceTokenById(id);
-        for(CrawlingSite site : keyword.getSiteList()){
-            fcmSender.unsubscribe(tokenList,
-                    enConverter.ktoe(keyword.getName()) + site.getCode().toString());
-            System.out.println(enConverter.ktoe(keyword.getName()) + site.getCode().toString());
-        }
     }
 
     @Override
     public void modifySubscription(List<CrawlingSite> oldSite,
                                    List<CrawlingSite> newSite,
                                    Long id, String keywordName) throws Exception{
-        System.out.println("구독 해제 목록");
-        System.out.println(oldSite);
-        System.out.println("새 구독 목록");
-        System.out.println(newSite);
-
-        Keyword keyword = Keyword.builder()
-                .name(keywordName)
-                .siteList(oldSite)
-                .build();
-
-        //기존 키워드 구독 해제
-        this.unsubscribe(keyword, id);
-
-        //신규 키워드 구독
-        keyword.setSiteList(newSite);
-        this.subscribe(keyword, id);
     }
 }
